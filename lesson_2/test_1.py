40bceb70d846a1b282067b1fb1895532f50ed4a8
Alena141005 <aantipova05@gmail.com>
2026-04-30 00:20:16 +0300

    Update test_1.py

--- lesson_2/test_1.py
+++ lesson_2/test_1.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('orders.csv', sep=',', encoding='utf-8')
+
+filt_df = df[(df['total'] >= 30) & (df['total'] <= 40)]
+filt_df = filt_df[filt_df['order_date'] >= '2023-01-01']
+
+print(filt_df[['order_id']])
 
