24803e5dbdd07a32cb7edf3f87461005564d12b0
Alena141005 <aantipova05@gmail.com>
2026-04-30 09:58:26 +0000

    save

--- lesson_2/test_1.py
+++ lesson_2/test_1.py
@@ -1,10 +1,11 @@
+#Задание_1
 import pandas as pd
 
 df = pd.read_csv('data/orders.csv', sep=',', encoding='utf-8')
+# df['order_date'] = pd.to_datetime(df['order_date'])
 
 filt_df = df[(df['total'] >= 30) & (df['total'] <= 40)]
 filt_df = filt_df[filt_df['order_date'] >= '2023-01-01']
 
 print(filt_df[['order_id']])
-
 
