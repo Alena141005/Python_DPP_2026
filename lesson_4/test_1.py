34ef7f58ad3294458700b48daeab1b34c73cb81a
Alena141005 <aantipova05@gmail.com>
2026-04-30 08:38:11 +0000

    save

--- lesson_4/test_1.py
+++ lesson_4/test_1.py
@@ -1,1 +1,11 @@
+# Задание 1
+# Используйте файл group_orders.csv. Посчитайте общее количество заказов в каждом
+# городе.
 
+import pandas as pd
+
+df = pd.read_csv('data/group_orders.csv', delimiter=',')
+
+#print(df.info())
+cities = df.groupby('city')['order_id'].count()
+print(cities)
--- lesson_4/test_2.py
+++ lesson_4/test_2.py
@@ -1,1 +1,9 @@
+# Задание 2
+# Найдите среднюю сумму заказа по каждому городу.
 
+import pandas as pd
+
+df = pd.read_csv('data/group_orders.csv', delimiter=',')
+
+cities = df.groupby('city')['total'].mean()
+print(cities)
